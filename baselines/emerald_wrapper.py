9a39628b386d3126742669dad7a0e569ab30c32d
dvruette <dimitrivr@icloud.com>
2023-11-19 14:21:14 +0100

    add healing reward and reward for revisiting old locations

--- baselines/emerald_wrapper.py
+++ baselines/emerald_wrapper.py
@@ -12,6 +12,131 @@ from pygba.game_wrappers.pokemon_emerald import (
 )
 
 
+class ExplorationTracker:
+    def __init__(
+        self,
+        distance_threshold: float = 6.0,  # GBA screen is 7x5 tiles
+        revisit_cooldown: int = 8192,
+        max_hnsw_count: int = 100_000,
+    ):
+        self.distance_threshold = distance_threshold
+        self.revisit_cooldown = revisit_cooldown
+        self.max_hnsw_count = max_hnsw_count
+        self.location_store = {}
+        self.cooldown_store = {}
+
+        self.curr_step = 0
+        self.total_visits = 0
+
+    def reset(self):
+        self.location_store = {}
+        self.cooldown_store = {}
+        self.curr_step = 0
+
+    def add_location(self, map_id, x, y):
+        if map_id not in self.location_store:
+            self.location_store[map_id] = hnswlib.Index(space='l2', dim=2)
+            self.location_store[map_id].init_index(max_elements=self.max_hnsw_count, ef_construction=100, M=16)
+
+        index = self.location_store[map_id]
+        pos = np.array([x, y])
+        label = index.get_current_count()
+        pos_hash = hash((map_id, label))
+        if index.get_current_count() == 0:
+            index.add_items(pos, np.array([pos_hash]))
+            self.cooldown_store[pos_hash] = self.curr_step + self.cooldown_steps
+            self.total_visits += 1
+        else:
+            labels, distances = index.knn_query(pos, k=1)
+            label, dist = labels[0][0], distances[0][0]
+            if dist < self.distance_threshold:
+                if self.cooldown_store[label] > self.curr_step:
+                    self.cooldown_store[label] = self.curr_step + self.cooldown_steps
+                    self.total_visits += 1
+            else:  # dist >= self.distance_threshold
+                index.add_items(pos, np.array([pos_hash]))
+                self.cooldown_store[pos_hash] = self.curr_step + self.cooldown_steps
+                self.total_visits += 1
+    
+    def update(self, location=None, pos=None):
+        if location is not None and pos is not None:
+            map_id = (location["mapGroup"], location["mapNum"])
+            if map_id != (0, 0):  # map_id is set to 0 while loading a new area
+                self.add_location(map_id, pos["x"], pos["y"])
+        
+        self.curr_step += 1
+
+    def total_size(self):
+        return sum(index.get_current_count() for index in self.location_store.values())
+    
+    def total_visited(self):
+        return self.total_visits
+
+
+class HealingTracker:
+    def __init__(
+        self,
+        consistency_threshold: int = 4,
+    ):
+        self.consistency_threshold = consistency_threshold
+
+        self.reset()
+
+    def reset(self):
+        self.curr_party = {}
+        self.prev_party = {}
+        self.candidate_party = {}
+        self.curr_consistency = 0
+        self.total_healed_amount = 0
+
+    def update(self, party):
+        is_consistent = True
+        seen_ids = set()
+        for mon in party:
+            mon_id = mon["box"]["personality"]
+            seen_ids.add(mon_id)
+
+            mon_hp = mon["hp"]
+            max_hp = mon["maxHp"]
+            if mon_id not in self.candidate_party:
+                self.candidate_party[mon_id] = (mon_hp, max_hp)
+                is_consistent = False
+            elif self.candidate_party[mon_id] != (mon_hp, max_hp):
+                self.candidate_party[mon_id] = (mon_hp, max_hp)
+                is_consistent = False
+
+        for mon_id in self.candidate_party.keys():
+            if mon_id not in seen_ids:
+                del self.candidate_party[mon_id]
+                is_consistent = False
+
+        if is_consistent:
+            self.curr_consistency += 1
+        if self.curr_consistency >= self.consistency_threshold:
+            self.curr_party = self.candidate_party.copy()
+            self.curr_consistency = 0
+            self.candidate_party = {}
+
+        for mon_id in self.curr_party.keys():
+            if mon_id not in seen_ids:
+                del self.curr_party[mon_id]
+
+        for mon_id in self.curr_party.keys():
+            if mon_id in self.prev_party():
+                prev_hp, prev_max_hp = self.prev_party[mon_id]
+                curr_hp, curr_max_hp = self.curr_party[mon_id]
+                if prev_hp > 0:  # don't count healing from fainted pokemon
+                    prev_missing_hp = prev_max_hp - prev_hp
+                    curr_missing_hp = curr_max_hp - curr_hp
+                    healed_amount = max(0, prev_missing_hp - curr_missing_hp)
+                    self.total_healed_amount += healed_amount / curr_max_hp
+
+        self.prev_party = self.curr_party.copy()
+
+    def total_healed(self):
+        return self.total_healed_amount
+
+
 class CustomEmeraldWrapper(GameWrapper):
     def __init__(
         self,
@@ -19,23 +144,26 @@ class CustomEmeraldWrapper(GameWrapper):
         pokedex_reward: float = 10,
         pokenav_reward: float = 10,
         champion_reward: float = 100.0,
-        visit_city_reward: float = 10.0,
-        money_gained_reward: float = 0,
+        visit_city_reward: float = 5.0,
+        money_gained_reward: float = 2e-4,
         # setting this below half of money_gained_reward leaves an exploit
         # where the agent can just buy and sell stuff at the pokemart..
         # but setting it too high will make it scared of battling
-        money_lost_reward: float = 0,
+        money_lost_reward: float = 1e-4,
         seen_pokemon_reward: float = 0.2,
         caught_pokemon_reward: float = 1.0,
-        trainer_beat_reward: float = 3.0,
+        trainer_beat_reward: float = 2.0,
         event_reward: float = 0.075,
-        exp_reward_transform: Literal["linear", "sqrt", "log"] = "log",
-        exp_reward_shape: float = 0.5,
-        exp_reward_scale: float = 0.5,
+        exp_reward_transform: Literal["linear", "sqrt", "log", "tanh"] = "tanh",
+        exp_reward_shape: float = 0.003,
+        exp_reward_scale: float = 5,
+        heal_reward: float = 0.05,
         exploration_reward: float = 0.02,
+        revisit_reward: float = 0.01,
+        revisit_cooldown: int = 8192,
         exploration_dist_thresh: float = 6.0,  # GBA screen is 7x5 tiles
-        max_hnsw_count: int = 100000,
-        reward_scale: float = 0.1,
+        max_hnsw_count: int = 100_000,
+        reward_scale: float = 1.0,
     ):
         self.badge_reward = badge_reward
         self.pokedex_reward = pokedex_reward
@@ -51,40 +179,22 @@ class CustomEmeraldWrapper(GameWrapper):
         self.exp_reward_transform = exp_reward_transform
         self.exp_reward_shape = exp_reward_shape
         self.exp_reward_scale = exp_reward_scale
+        self.heal_reward = heal_reward
+        self.exploration_reward = exploration_reward
+        self.revisit_reward = revisit_reward
         self.reward_scale = reward_scale
 
-        self.exploration_reward = exploration_reward
-        self.exploration_dist_thresh = exploration_dist_thresh
-        self.max_hnsw_count = max_hnsw_count
-
+        self.exploration_tracker = ExplorationTracker(
+            distance_threshold=exploration_dist_thresh,
+            revisit_cooldown=revisit_cooldown,
+            max_hnsw_count=max_hnsw_count,
+        )
         self._total_script_flags = 0
         self._prev_reward = 0.0
         self._game_state = {}
         self._prev_game_state = {}
         self._reward_info = {}
         self._location_store = {}
-
-    def get_exploration_reward(self, state):
-        location = state.get("location", None)
-        pos = state.get("pos", None)
-        if location is not None and pos is not None:
-            map_id = (location["mapGroup"], location["mapNum"])
-            if map_id != (0, 0):  # map_id is set to 0 while loading a new area
-                if map_id not in self._location_store:
-                    self._location_store[map_id] = hnswlib.Index(space='l2', dim=2)
-                    self._location_store[map_id].init_index(max_elements=self.max_hnsw_count, ef_construction=100, M=16)
-                index = self._location_store[map_id]
-                pos = np.array([pos["x"], pos["y"]])
-                if index.get_current_count() == 0:
-                    index.add_items(pos, np.array([index.get_current_count()]))
-                else:
-                    labels, distances = index.knn_query(pos, k = 1)
-                    if distances[0][0] >= self.exploration_dist_thresh:
-                        # print(f"distances[0][0] : {distances[0][0]} similar_frame_dist : {self.similar_frame_dist}")
-                        index.add_items(
-                            pos, np.array([index.get_current_count()])
-                        )
-        return sum(index.get_current_count() for index in self._location_store.values()) * self.exploration_reward
 
     def reward(self, gba: PyGBA, observation):
         self._game_state.update(get_game_state(gba))
@@ -93,7 +203,10 @@ class CustomEmeraldWrapper(GameWrapper):
         # Game state can get funky during loading screens, so we just wait until
         # we get a valid observation.
         if observation is not None and observation.sum() < 1:
+            self.exploration_tracker.update()
             return 0.0
+        
+        self.exploration_tracker.update(state.get("location", None), state.get("pos", None))
 
         trainer_flags = state.get("trainer_flags", None)
         num_trainer_flags = count_flags(trainer_flags)
@@ -114,6 +227,10 @@ class CustomEmeraldWrapper(GameWrapper):
                 exp_reward = self.exp_reward_scale * total_gained_exp ** self.exp_reward_shape
             elif self.exp_reward_transform == "log":
                 exp_reward = self.exp_reward_scale * np.log(self.exp_reward_shape * total_gained_exp + 1)
+            elif self.exp_reward_transform == "tanh":
+                exp_reward = self.exp_reward_scale * np.tanh(self.exp_reward_shape * total_gained_exp)
+            # round exp reward to 0.01
+            exp_reward = round(exp_reward * 100) / 100
         else:
             print()
             print(f"WARNING: total_gained_exp >= 0: {total_gained_exp}")
@@ -125,18 +242,29 @@ class CustomEmeraldWrapper(GameWrapper):
         # player starts with $3000 cash
         earned_money = (state.get("money", 3000) - 3000)
         # in case the the game state glitches and gives the player a lot of money, we ignore values over 100k
-        if earned_money > 0 and earned_money < 100_000:
-            money_rew = earned_money * self.money_gained_reward
-        elif earned_money < 0:
-            money_rew = earned_money * self.money_lost_reward
+        if earned_money != 0 and (self.money_gained_reward != 0.0 or self.money_lost_reward != 0.0):
+            if earned_money > 0 and earned_money < 100_000:
+                money_rew = earned_money * self.money_gained_reward
+            elif earned_money < 0:
+                money_rew = earned_money * self.money_lost_reward
         else:
             money_rew = 0.0
+
+        # exploration reward
+        total_visited = self.exploration_tracker.total_visited()
+        total_size = self.exploration_tracker.total_size()
+        num_revisited = total_visited - total_size
+
+        # heal reward
+        healed_amount = self.healing_tracker.total_healed()
 
         self._reward_info = {
             "visit_city_rew": visited_cities * self.visit_city_reward,
             "seen_poke_rew": state.get("num_seen_pokemon", 0) * self.seen_pokemon_reward,
             "caught_poke_rew": state.get("num_caught_pokemon", 0) * self.caught_pokemon_reward,
-            "explore_rew": self.get_exploration_reward(state),
+            "explore_rew": total_size * self.exploration_reward,
+            "revisit_rew": num_revisited * self.revisit_reward,
+            "heal_rew": healed_amount * self.heal_reward,
             "money_rew": money_rew,
             "pokedex_rew": (1.0 if state.get("has_pokedex", False) else 0.0) * self.pokedex_reward,
             "pokenav_rew": (1.0 if state.get("has_pokenav", False) else 0.0) * self.pokenav_reward,
@@ -155,6 +283,7 @@ class CustomEmeraldWrapper(GameWrapper):
         return self.reward_scale * (reward - prev_reward)
     
     def reset(self, gba: PyGBA):
+        self.exploration_tracker.reset()
         self._game_state = {}
         self._location_store = {}
         self._total_script_flags = 0
